bd59d7006668f6e76d2753621c60018aca00f5c0
Yujie Zhu <yzhu@Yujies-MacBook-Pro.local>
2022-04-25 15:31:47 -0700

    added multithread

--- MyServer.java
+++ MyServer.java
@@ -2,8 +2,6 @@ import java.net.*;
 import java.io.*;
 import java.nio.file.*;
 
-
-
 public class MyServer {
     public static void main(String[] args) throws Exception {
         String root_path;
@@ -13,12 +11,19 @@ public class MyServer {
         root_path = parsedArgs[0];
         port = Integer.parseInt(parsedArgs[1]);
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
             while (true) {
-                try (Socket client = serverSocket.accept()) {
-                    threadWork(client, root_path);
+                try  {
+                    Socket client = serverSocket.accept();
+                    Worker worker = new Worker(client, root_path);
+                    worker.start();
+                }catch(Exception e){
+                    System.err.println("Creating worker error:"+e);
                 }
             }
+        }catch (Exception ee){
+            System.err.println("Creating Server socket error:"+ee);
         }
     }
 
@@ -35,53 +40,83 @@ public class MyServer {
         }
         return new String[]{root_path, port};
     }
-    private static void threadWork(Socket client, String root_path) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        StringBuilder requestBuilder = new StringBuilder();
-        String line;
-        while (!(line = br.readLine()).isBlank()) {
-            requestBuilder.append(line + "\r\n");
+
+}
+
+class Worker extends Thread{
+    Socket client;
+    String root_path;
+
+    public Worker(Socket client, String root_path){
+        this.client = client;
+        try {
+        }catch(Exception e){
+            System.out.println("Constructor error:");
+            System.out.println(e);
         }
-        String request = requestBuilder.toString();
-        String[] splitedRequest = request.split("\r\n");
-        String[] firstLine = splitedRequest[0].split("\\s+");
-        String method = firstLine[0];
-        String path = firstLine[1];
-
-        Path filePath = getFilePath(path, root_path);
-
-
-        if (Files.exists(filePath)) {
-            String contentType = getContentType(filePath);
-            sendResponse(client, "200 OK", contentType, Files.readAllBytes(filePath));
-        } else {
-            sendResponse(client, "404 Not Found", "text/html", "<h1><b>404 Not Found</b></h1>".getBytes());
-        }
-
+        this.root_path = root_path;
     }
 
-    private static void sendResponse(Socket client, String status, String contentType, byte[] content)
+    public void run(){
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+            StringBuilder requestBuilder = new StringBuilder();
+            String line;
+            while (!(line = br.readLine()).isBlank()) {
+                requestBuilder.append(line + "\r\n");
+            }
+            String request = requestBuilder.toString();
+            System.out.println(request);
+            String[] splitedRequest = request.split("\r\n");
+            String[] firstLine = splitedRequest[0].split("\\s+");
+            String method = firstLine[0];
+            String path = firstLine[1];
+            Path filePath = this.getFilePath(path);
+            System.out.println(filePath);
+
+
+            if (!method.equals("GET")){
+                this.sendResponse("403 Forbidden", "text/html", "<h1><b>403 Forbidden</b></h1>".getBytes());
+            } else if (Files.exists(filePath)) {
+                String contentType = getContentType(filePath);
+                this.sendResponse("200 OK", contentType, Files.readAllBytes(filePath));
+            } else {
+                this.sendResponse("404 Not Found", "text/html", "<h1><b>404 Not Found</b></h1>".getBytes());
+            }
+        }catch (Exception e){
+            System.err.println(e);
+            try {
+                this.sendResponse("400 Bad Request", "text/html", "<h1><b>400 Bad Request</b></h1>".getBytes());
+            }catch (IOException ee){}
+        }
+    }
+
+    private void sendResponse(String status, String contentType, byte[] content)
             throws IOException {
-        OutputStream clientOutput = client.getOutputStream();
+        OutputStream clientOutput = this.client.getOutputStream();
         clientOutput.write(("HTTP/1.0 "+status+"\r\n").getBytes());
         clientOutput.write(("ContentType: "+contentType+"\r\n").getBytes());
+        clientOutput.write(("ContentLength: "+content.length+"\r\n").getBytes());
+        clientOutput.write(("Date: "+(new java.util.Date()).toString()+"\r\n").getBytes());
         clientOutput.write("\r\n".getBytes());
         clientOutput.write(content);
         clientOutput.write("\r\n\r\n".getBytes());
         clientOutput.flush();
-        client.close();
+        System.out.println("Closing socket......\r\n");
+        System.out.println("------------------------------------------------\r\n");
+        this.client.close();
     }
 
-    private static String getContentType(Path filePath) throws IOException {
+    private String getContentType(Path filePath) throws IOException {
         return Files.probeContentType(filePath);
     }
 
 
-    private static Path getFilePath(String path, String root_path) {
+    private Path getFilePath(String path) {
         if ("/".equals(path)) {
             path = "/index.html";
         }
-        String abs_path = root_path + path;
+        String abs_path = this.root_path + path;
         return Paths.get(abs_path);
     }
 }
